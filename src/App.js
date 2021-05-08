3da7600c056cc62dbe661efc3f37b36668456dc0
John <johnholl@umich.edu>
2021-05-08 07:31:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Row, Col, InputNumber, Button, Input} from 'antd';
 import './App.css';
+import Plot from 'react-plotly.js';
+import {generatePath, calculateOffsetPaths} from './utils';
+import 'antd/dist/antd.css';
+
+
+
 
 function App() {
+  const [x, setX] = useState([]);
+  const [y, setY] = useState([]);
+  const [xLeft, setLeftX] = useState([]);
+  const [yLeft, setLeftY] = useState([]);
+  const [xRight, setRightX] = useState([]);
+  const [yRight, setRightY] = useState([]);
+
+  const [inputs, setInputs] = useState([{t:0, x:0, y:0, xp:0, yp:1, dt:0.1}, {t:1, x:1, y:1, xp:0, yp:1, dt:0.1}])
+
+  const onChange = (label, val, idx) => {
+    var newinputs = JSON.parse(JSON.stringify(inputs));
+    var newdata = {...inputs[idx], [label]: val}
+    newinputs[idx] = newdata;
+    setInputs(newinputs)
+  }
+
+  const calculate = () => {
+    var xway = inputs.map(a=>{return(a.x)})
+    var yway = inputs.map(a=>{return(a.y)})
+    var xpway = inputs.map(a=>{return(a.xp)})
+    var ypway = inputs.map(a=>{return(a.yp)})
+    var tway = inputs.map(a=>{return(a.t)})
+    var dtway = inputs.map(a=>{return(a.dt)})
+    console.log(xway, yway, xpway, ypway, tway, dtway);
+    const {xpath, ypath} = generatePath(xway, yway, xpway, ypway, tway, dtway);
+    const {xLpath, yLpath, xRpath, yRpath} = calculateOffsetPaths(xpath, ypath, 0.5);
+  
+    setX(xpath);
+    setY(ypath);
+    setLeftX(xLpath);
+    setLeftY(yLpath);
+    setRightX(xRpath);
+    setRightY(yRpath);
+
+  }
+
+  const addPoint = () => {
+    var newinputs = JSON.parse(JSON.stringify(inputs));
+    var newdata = inputs[inputs.length-1];
+    newinputs.push(newdata);
+    setInputs(newinputs);
+  }
+
+  const removePoint = (i) => {
+    var newinputs = JSON.parse(JSON.stringify(inputs));
+    newinputs.pop()
+    setInputs(newinputs);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row style={{padding:20}} gutter={[16,16]}>
+      <Col span={12}>
+        {inputs.map((value, i) => {
+          return(
+            <Row style={{padding:20}} align="middle" justify="space-around" key={i}>
+            <Col span={2}>
+              <Row>t</Row>
+              <Row><InputNumber stringmode label={"t"} min={0} max={100} defaultValue={inputs[i].t} placeholder={"t"} onChange={(val) => {onChange("t", val, i)}} /></Row>
+            </Col>
+            <Col span={2}>
+              <Row>x</Row>
+              <Row><InputNumber label={"x"} min={-10} max={10} defaultValue={inputs[i].x} placeholder={"x"} onChange={(val) => {onChange("x", val, i)}} /></Row>
+            </Col>
+            <Col span={2}>
+              <Row>y</Row>
+              <Row><InputNumber label={"y"} min={-10} max={10} defaultValue={inputs[i].y} placeholder={"y"} onChange={(val) => {onChange("y", val, i)}} /></Row>
+            </Col>
+            <Col span={2}>
+              <Row>x vel</Row>
+              <Row><InputNumber label={"xp"} min={-10} max={10} defaultValue={inputs[i].xp} placeholder={"x vel"} onChange={(val) => {onChange("xp", val, i)}} /></Row>
+            </Col>
+            <Col span={2}>
+              <Row>y vel</Row>
+              <Row><InputNumber label={"yp"} min={-10} max={10} defaultValue={inputs[i].yp} placeholder={"y vel"} onChange={(val) => {onChange("yp", val, i)}} /></Row>
+            </Col>
+            <Col span={2}>
+              <Row>dt</Row>
+              <Row><InputNumber label={"dt"} min={0} max={10} defaultValue={inputs[i].dt} placeholder={"dt"} onChange={(val) => {onChange("dt", val, i)}} /></Row>
+            </Col>
+            <Col span={2}>
+              <Row><Button onClick={removePoint}>Remove</Button></Row>
+            </Col> 
+          </Row>
+          );
+        })}
+        <Row justify="space-around">
+        <Button type="primary" onClick={addPoint}>Add Point</Button>
+        <Button onClick={calculate}>Calculate Path</Button>
+        </Row>
+        </Col>
+        <Col span={12}>
+        <Plot 
+        data={[
+          {
+            x: x,
+            y: y,
+            type: 'scatter',
+            mode: 'markers',
+            marker: {color: 'red'},
+          },
+          {
+            x: xLeft,
+            y: yLeft,
+            type: 'scatter',
+            mode: 'markers',
+            marker: {color: 'blue'},
+          },
+          {
+            x: xRight,
+            y: yRight,
+            type: 'scatter',
+            mode: 'markers',
+            marker: {color: 'green'},
+          },
+           ]}
+        layout={ {width: 500, height: 500, title: 'Robot path', xaxis: {range: [-10,10]}, yaxis: {range: [-10, 10]}}}
+        config = {{scrollZoom:true}}
+      />
+        </Col>
+      </Row>
+
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,79 @@
+import {matrix, inv, pow, multiply} from 'mathjs';
+
+
+export function spline(t0, t1, x0, x1, y0, y1, xp0, xp1, yp0, yp1) {
+
+
+    const m = matrix([[pow(t0,3), pow(t0,2), t0, 1],
+                      [pow(t1,3), pow(t1,2), t1, 1],
+                      [multiply(pow(t0,2),3), multiply(t0,2), 1, 0],
+                      [multiply(pow(t1,2),3), multiply(t1,2), 1, 0]])
+    const minv = inv(m);
+
+    const xcoeff = multiply(minv, [x0,x1,xp0,xp1])
+    const ycoeff = multiply(minv, [y0,y1,yp0,yp1])
+
+    return {xcoeff, ycoeff};
+
+
+}
+
+export function generatePoints(t0, t1, dt, xcoeff, ycoeff) {
+
+    var t = t0;
+    var xpoints = [];
+    var ypoints = [];
+    while(t < t1 + multiply(dt, 0.1)){
+        var xval = multiply(xcoeff.get([0]),pow(t,3))+multiply(xcoeff.get([1]),pow(t,2))+multiply(xcoeff.get([2]),t)+xcoeff.get([3]);
+        var yval = multiply(ycoeff.get([0]),pow(t,3))+multiply(ycoeff.get([1]),pow(t,2))+multiply(ycoeff.get([2]),t)+ycoeff.get([3]);
+        xpoints.push(xval);
+        ypoints.push(yval);
+        t = t + dt;
+    }
+
+    return {xpoints, ypoints};
+
+}
+
+// precondition x, y, xp, yp, t, dt are all arrays of the same length
+export function generatePath(x, y, xp, yp, t, dt) {
+
+    var xpath = [];
+    var ypath = [];
+
+    for(var i =0; i<x.length-1; i++){
+        const {xcoeff, ycoeff} = spline(t[i], t[i+1], x[i], x[i+1], y[i], y[i+1], xp[i], xp[i+1], yp[i], yp[i+1])
+        const {xpoints, ypoints} = generatePoints(t[i], t[i+1], dt[i], xcoeff, ycoeff);
+        xpath = xpath.concat(xpoints);
+        ypath = ypath.concat(ypoints);
+    }
+
+    return {xpath, ypath}
+
+}
+
+
+export function calculateOffsetPaths(x, y, d){
+    var xLpath = [];
+    var yLpath = [];
+    var xRpath = [];
+    var yRpath = [];
+
+    for(var i=0; i<x.length-1; i++){
+        var xdiff = x[i+1] - x[i];
+        var ydiff = y[i+1] - y[i];
+        var r = pow(pow(xdiff, 2) + pow(ydiff, 2), 0.5)
+        var newxL = x[i] + ydiff*d/r;
+        var newyL = y[i] - xdiff*d/r;
+        xLpath.push(newxL);
+        yLpath.push(newyL);
+
+        var newxR = x[i] - ydiff*d/r;
+        var newyR = y[i] + xdiff*d/r;
+        xRpath.push(newxR);
+        yRpath.push(newyR);
+
+    }
+
+    return {xLpath, yLpath, xRpath, yRpath};
+}
